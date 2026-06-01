c85980cf053ec71018689ca2c20d0a94b1332953
jagadeeshvedikasetty <jagadeeshvedikasetty@gmail.com>
2026-06-01 23:09:19 +0530

    Capgo onboarding setup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.aspirant.app',
   appName: 'aspirant-client',
-  webDir: 'build'
+  webDir: 'build',
+  plugins: {
+    extConfig: {},
+    CapacitorUpdater: {
+      appId: 'com.aspirant.app'
+    }
+  }
 };
 
 export default config;
